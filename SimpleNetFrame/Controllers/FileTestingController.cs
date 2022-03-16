61c0743abf4312f82d64edfba5664af90ab260fe
wasuwat-kubeops <wasuwat.p@kubeops.guru>
2022-03-16 15:20:27 +0700

    Add: dump error

--- SimpleNetFrame/Controllers/FileTestingController.cs
+++ SimpleNetFrame/Controllers/FileTestingController.cs
@@ -12,20 +12,28 @@ namespace SimpleNetFrame.Controllers
     {
         public string Get()
         {
+            try
+            {
+                ShareClient share = new ShareClient("FileEndpoint=https://wasuwat.file.core.windows.net", "test-file-share");
+            
+                share.CreateIfNotExists();
 
-            ShareClient share = new ShareClient("FileEndpoint=https://wasuwat.file.core.windows.net", "test-file-share");
-            share.CreateIfNotExists();
+                var superDir = share.GetDirectoryClient("super");
+                superDir.CreateIfNotExists();
 
-            var superDir = share.GetDirectoryClient("super");
-            superDir.CreateIfNotExists();
+                var fileClient = superDir.GetFileClient("ex_"+DateTime.Now.ToString("dd-MM-yy_HH-mm-ss"));
 
-            var fileClient = superDir.GetFileClient("ex_"+DateTime.Now.ToString("dd-MM-yy_HH-mm-ss"));
+                MemoryStream ms = new MemoryStream(System.Text.Encoding.ASCII.GetBytes("HelloWord"));
+                fileClient.Create(ms.Length);
+                fileClient.UploadRange(new Azure.HttpRange(0, ms.Length), ms);
 
-            MemoryStream ms = new MemoryStream(System.Text.Encoding.ASCII.GetBytes("HelloWord"));
-            fileClient.Create(ms.Length);
-            fileClient.UploadRange(new Azure.HttpRange(0, ms.Length), ms);
+                return "Ok";
 
-            return "Ok";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
     }
 }
